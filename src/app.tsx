fea18307c787d336973a6367b3214879c65da96b
Nicolas Freitas <nicolasfpdev@gmail.com>
2024-02-07 01:28:42 -0300

    estilizando cards, preparando para receber inputs do usuário

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: ''
+}
+
 export function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -11,14 +16,12 @@ export function App() {
           type="text"
           placeholder='Busque em suas notas...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'/>
-        </form>
-        <div className='h-px bg-slate-700' />
-        <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
-          <NewNoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-        </div>
+      </form>
+      <div className='h-px bg-slate-700' />
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
+        <NewNoteCard />
+        <NoteCard note={note}/>
+      </div>
     </div>
   )
 }
