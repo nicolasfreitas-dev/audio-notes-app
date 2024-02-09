62eee2a9c076908a91a564c0f3910af618177c36
Nicolas Freitas <nicolasfpdev@gmail.com>
2024-02-09 01:30:11 -0300

    finalizando projeto

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,13 +5,15 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
   note: {
+    id: string
     date: Date
     content: string
   }
+
+  onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps)
-{
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
     return (
       <Dialog.Root>
         <Dialog.Trigger className='rounded-md text-left outline-none bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 flex flex-col'>
@@ -23,15 +25,17 @@ export function NoteCard({ note }: NoteCardProps)
             </p>
               <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
         </Dialog.Trigger>
+
         <Dialog.Portal>
           <Dialog.Overlay className='inset-0 fixed bg-black/50'/>
-          <Dialog.Content className='z-10 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none overflow-hidden'>
+          <Dialog.Content className='z-10 fixed inset-0 md:inset-auto md:left-1/2 md:op-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:h-[60vh] w-full bg-slate-700 md:rounded-md flex flex-col outline-none overflow-hidden'>
             <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
               <X className='size-5'/>
             </Dialog.Close>
             <div className='flex flex-1 flex-col gap-3 p-5'>
               <span className='text-sm font-medium text-slate-300'>
-                {note.date.toISOString()}
+                {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+
               </span>
               <p className='text-sm leading-6 text-slate-400'>
                 {note.content}
@@ -42,7 +46,7 @@ export function NoteCard({ note }: NoteCardProps)
             type='button'
             className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'>
 
-              Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?
+              Deseja <span onClick={() => onNoteDeleted(note.id)} className='text-red-400 group-hover:underline'>apagar essa nota</span>?
             </button>
           </Dialog.Content>
         </Dialog.Portal>
